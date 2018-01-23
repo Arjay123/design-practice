a774d1dca22eff7a89f3d1ba0b4213f534cb34d4
arjaynguyen <arjaynguyen123@hotmail.com>
2018-01-23 15:22:25 -0800

    event listeners for dropdowns

--- dailyui/007-settings/js/index.js
+++ dailyui/007-settings/js/index.js
@@ -1,3 +1,9 @@
 (function(){
 
+    var $dropdowns = $(".dropdown");
+    $dropdowns.on('click', function(event) {
+        var input = $(this).find(".dropdown-input");
+        input.prop('checked', !(input.prop('checked')));
+    })
+
 })();
