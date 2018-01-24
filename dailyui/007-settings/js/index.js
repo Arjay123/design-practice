e54ccf5de66fc96762c203cee7deff6036d25509
arjaynguyen <arjaynguyen123@hotmail.com>
2018-01-23 16:04:34 -0800

    dropdowns working

--- dailyui/007-settings/js/index.js
+++ dailyui/007-settings/js/index.js
@@ -4,6 +4,8 @@
     $dropdowns.on('click', function(event) {
         var input = $(this).find(".dropdown-input");
         input.prop('checked', !(input.prop('checked')));
-    })
+
+        $(this).find('.dropdown-window').toggle(200);
+    });
 
 })();
