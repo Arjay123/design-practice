1ecb2e301f56ff6d43e7fae726356c7938e71b3f
arjaynguyen <arjaynguyen123@hotmail.com>
2018-02-14 15:07:35 -0800

    fix dropdown

--- dailyui/007-settings/js/index.js
+++ dailyui/007-settings/js/index.js
@@ -4,8 +4,13 @@
     $dropdowns.on('click', function(event) {
         var input = $(this).find(".dropdown-input");
         input.prop('checked', !(input.prop('checked')));
+        if (input.prop('checked')) {
+            $(this).find('.dropdown-window').show(200);
+        }
+        else {
+            $(this).find('.dropdown-window').hide(200);
+        }
 
-        $(this).find('.dropdown-window').toggle(200);
     });
 
 })();
